5db37054299704c7a2cf98d509c182a86520e719
John Smethurst <johnsmethurst@gmail.com>
2016-10-27 10:34:15 +0100

    Included random sampling from each file and output to df with line number

--- 02_clean_sample.R
+++ 02_clean_sample.R
@@ -1,4 +1,18 @@
-# Extracting the number of lines from each file
+#######################################################
+# Setting random seeds and sample index for each file #
+#######################################################
+
+# Number of lines to read
+nl <- 1000
+seed <- 100
+set.seed(seed)
+twitter_index <- sample(1:nl_twitter,nl,replace=FALSE)
+news_index <- sample(1:nl_news,nl,replace=FALSE)
+blogs_index <- sample(1:nl_blogs,nl,replace=FALSE)
+
+#################################################
+# Extracting the number of lines from each file #
+#################################################
 
 system("wc -l data/final/en_US/en_US.twitter.txt > data/lines.txt")
 system("wc -l data/final/en_US/en_US.news.txt >> data/lines.txt")
@@ -13,30 +27,84 @@ nl_news <- as.numeric(strsplit(nl_content," ")[[2]][1])
 nl_blogs <- as.numeric(strsplit(nl_content," ")[[3]][1])
 close(nl_con)
 
-# Parameters
+#####################################
+# Extracting twitter data           #
+#####################################
 
-# Number of lines read
-nl = 1000
+twitter_con <- file('data/final/en_US/en_US.twitter.txt', 'r')
+twitter_lines <- rep("",nl)
+twitter_line_num <- rep(0,nl)
 
-# reading data
-twitter_con <- file('data/final/en_US/en_US.twitter.txt', 'r',fileEncoding=getOption("latin1"))
-news_con <- file('data/final/en_US/en_US.news.txt', 'r',fileEncoding=getOption("latin1"))
-blogs_con <- file('data/final/en_US/en_US.blogs.txt', 'r',fileEncoding=getOption("latin1"))
-
-twitter_lines <-readLines(twitter_con,n=nl)
-news_lines <-readLines(news_con,n=nl)
-blogs_lines <-readLines(blogs_con,n=nl)
+j=0
+for (i in 1:nl_twitter) {
+     if (i %in% twitter_index){
+          j = j + 1
+          twitter_lines[j] <- readLines(twitter_con,n=1)
+          twitter_line_num[j] <- i
+     } 
+     else
+     {
+          temp <- readLines(twitter_con,n=1)
+     }
+}
 
 close(twitter_con)
+
+#####################################
+# Extracting news data              #
+#####################################
+
+news_con <- file('data/final/en_US/en_US.news.txt', 'r')
+news_lines <- rep("",nl)
+news_line_num <- rep(0,nl)
+
+j=0
+for (i in 1:nl_news) {
+     if (i %in% news_index){
+          j = j + 1
+          news_lines[j] <- readLines(news_con,n=1)
+          news_line_num[j] <- i
+     } 
+     else
+     {
+          temp <- readLines(news_con,n=1)
+     }
+}
+
 close(news_con)
+
+#####################################
+# Extracting blogs data             #
+#####################################
+
+blogs_con <- file('data/final/en_US/en_US.blogs.txt', 'r')
+blogs_lines <- rep("",nl)
+blogs_line_num <- rep(0,nl)
+
+j=0
+for (i in 1:nl_blogs) {
+     if (i %in% blogs_index){
+          j = j + 1
+          blogs_lines[j] <- readLines(blogs_con,n=1)
+          blogs_line_num[j] <- i
+     } 
+     else
+     {
+          temp <- readLines(blogs_con,n=1)
+     }
+}
+
 close(blogs_con)
 
-# Reading google "bad words" data
+#######################################
+# Removing bad words from each vector #
+#######################################
 
 badwords_con <- file('data/full-list-of-bad-words-banned-by-google-txt-file_2013_11_26_04_53_31_867.txt', 'r')
 badwords <- readLines(badwords_con)
 close(badwords_con)
 badwords <- append(badwords,"fuck")
+
 # Converting to ASCII
 badwords <- iconv(badwords, from = "", to="ASCII", sub="")
 # ordering badwords
@@ -51,16 +119,18 @@ for (word in badwords) {
      blogs_lines <- gsub(word,"",blogs_lines,ignore.case=TRUE)
 }
 
-# outputing data
+####################################################
+# Creating data frames and writting data to a file #
+####################################################
 
-twitter_con <- file('data/sample_data/en_US.twitter.txt', 'w')
-news_con <- file('data/sample_data/en_US.news.txt', 'w')
-blogs_con <- file('data/sample_data/en_US.blogs.txt', 'w')
+twitter_df <- data.frame(twitter_line_num,twitter_lines)
+news_df <- data.frame(news_line_num,news_lines)
+blogs_df <- data.frame(blogs_line_num,blogs_lines)
 
-writeLines(twitter_lines, con = twitter_con)
-writeLines(news_lines, con = news_con)
-writeLines(blogs_lines, con = blogs_con)
+twitter_fn <- paste("data/sample_data/twitter_",as.character(nl),"_",as.character(seed),".csv",sep="")
+news_fn <- paste("data/sample_data/news_",as.character(nl),"_",as.character(seed),".csv",sep="")
+blogs_fn <- paste("data/sample_data/blogs_",as.character(nl),"_",as.character(seed),".csv",sep="")
 
-close(twitter_con)
-close(news_con)
-close(blogs_con)
+write.csv(twitter_df,twitter_fn)
+write.csv(news_df,news_fn)
+write.csv(blogs_df,blogs_fn)
